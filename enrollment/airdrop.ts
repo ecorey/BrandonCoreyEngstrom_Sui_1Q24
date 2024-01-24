9c54b4a2d5f0d667be28966623dde3e890b372d7
ecorey <bengstrom32@gmail.com>
2024-01-23 19:50:12 -0500

    day1 commit

--- enrollment/airdrop.ts
+++ enrollment/airdrop.ts
@@ -1,25 +1,25 @@
 import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
 import { getFaucetHost, requestSuiFromFaucetV0 } from "@mysten/sui.js/faucet";
-import { fromHEX } from "@mysten/sui.js/utils";
+import wallet from "./dev-wallet.json";
 
-import wallet from "./dev-wallet.json"
+// Parse the privateKey string into an array of numbers
+const privateKeyArray = wallet.privateKey.split(',').map(num => parseInt(num, 10));
 
+// Convert the array of numbers into a Uint8Array
+const privateKeyBytes = new Uint8Array(privateKeyArray);
 
-// Convert the hex string to a Uint8Array
-const privateKeyBytes = fromHEX(wallet.privateKey);
-
-// We're going to import our keypair from the wallet file
-const keypair = Ed25519Keypair.fromSecretKey(new Uint8Array(privateKeyBytes));
+// Create the keypair
+const keypair = Ed25519Keypair.fromSecretKey(privateKeyBytes);
 
 (async () => {
     try {
         let res = await requestSuiFromFaucetV0({
             host: getFaucetHost("devnet"),
             recipient: keypair.toSuiAddress(),
-          });
-          console.log(`Success! Check our your TX here:
-          https://suiscan.xyz/devnet/object/${res.transferredGasObjects[0].id}`);
+        });
+        console.log(`Success! Check out your TX here:
+        https://suiscan.xyz/devnet/object/${res.transferredGasObjects[0].id}`);
     } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
+        console.error(`Oops, something went wrong: ${e}`);
     }
 })();
--- enrollment/info.md
+++ enrollment/info.md
@@ -1,15 +1,23 @@
 **All TESTNET**
 **Note Secure for Learning Only, Never expose PK**
+keygen:
 
-You've generated a new Sui wallet: 0xb3d4cb714181fec39c22d820c963da9cfac970d3ab77c464b0dea06ce673c3e5
+    You've generated a new Sui wallet: 0xb3d4cb714181fec39c22d820c963da9cfac970d3ab77c464b0dea06ce673c3e5
 
-To save your wallet, copy and paste the following into a JSON file:
+    To save your wallet, copy and paste the following into a JSON file:
 
-[205,135,82,122,92,191,230,26,186,193,84,37,231,224,134,137,88,91,210,10,145,131,234,192,175,104,252,61,220,0,124,86]
+    [205,135,82,122,92,191,230,26,186,193,84,37,231,224,134,137,88,91,210,10,145,131,234,192,175,104,252,61,220,0,124,86]
 
 
-You can use the below HEX to import the key into a web wallet:
+    You can use the below HEX to import the key into a web wallet:
 
-cd87527a5cbfe61abac15425e7e08689585bd20a9183eac0af68fc3ddc007c56
+    cd87527a5cbfe61abac15425e7e08689585bd20a9183eac0af68fc3ddc007c56
 
+airdrop:
+
+    Success! Check out your TX here:
+            https://suiscan.xyz/devnet/object/0xe549b151fa8e5230c015fd7e46c6d8beb11de90a729e77a4cbba4d045dbcb7d5
+
+
+![airdrop](./airdrop.png)
 
